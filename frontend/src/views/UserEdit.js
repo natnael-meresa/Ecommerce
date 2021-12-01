e34e308661c7a7015cac5a0d0c4c070d20f9bc00
natnael meresa <natnaelmeresa6@gmail.com>
2021-12-01 12:17:19 +0300

    adds users edit and list to the dashboard

---
+++ frontend/src/views/UserEdit.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { getUserDetails, updateUser } from "../actions/userActions";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import FormContainer from "../components/FormContainer";
+import SideBar from "../components/SideBar.js";
+import DashNav from "../components/DashNav.js";
+import { USER_UPDATE_RESET } from "../constants/userConstants";
+
+import {
+  Button,
+  Form,
+  Container,
+} from "react-bootstrap";
+
+const UserEdit = () => {
+    const { id } = useParams();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
+
+
+  const dispatch = useDispatch();
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+
+  const userUpdate = useSelector((state) => state.userUpdate);
+  const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = userUpdate;
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+      if(successUpdate){
+          dispatch({type: USER_UPDATE_RESET })
+          navigate('/dashboard/userlist')
+      }else{
+        if(!user.name || user._id !== id){
+            dispatch(getUserDetails(id))
+        }else{
+            setName(user.name)
+            setEmail(user.email)
+            setIsAdmin(user.isAdmin)
+        }
+      }
+  }, [user, id, dispatch, successUpdate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(updateUser({_id:id, name, email, isAdmin}))
+  };
+  return (
+    <>
+      <DashNav />
+      <SideBar />
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <FormContainer>
+          <h1>Edit User</h1>
+          {loadingUpdate && <Loader />}
+          {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
+          {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message>: (
+              <Form onSubmit={submitHandler}>
+              <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="isadmin">
+                <Form.Check
+                  type="checkbox"
+                  label= "Is Admin"
+                  checked={isAdmin}
+                  onChange={(e) => setIsAdmin(e.target.checked)}
+                ></Form.Check>
+              </Form.Group>
+              <Button type="submit" variant="primary">
+                Update
+              </Button>
+            </Form>
+  
+          ) }
+        </FormContainer>
+      </main>
+    </>
+  );
+};
+
+export default UserEdit
---
+++ frontend/src/views/UserList.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { listUsers, deleteUser } from "../actions/userActions";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { Table, Button, Container } from "react-bootstrap";
+import SideBar from "../components/SideBar.js";
+import DashNav from "../components/DashNav.js";
+const UserList = () => {
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate()
+
+  const userList = useSelector((state) => state.userList);
+  const { loading, error, users } = userList;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const userDelete = useSelector((state) => state.userDelete);
+  const { success: successDelete } = userDelete;
+
+  useEffect(() => {
+    if(userInfo && userInfo.isAdmin){
+      dispatch(listUsers());
+    }else{
+      navigate('/login')
+    }
+  }, [dispatch, successDelete,userInfo]);
+
+  const deleteUserHandler = (id) => {
+    if(window.confirm('Are you sure')){
+      dispatch(deleteUser(id))
+    }
+  };
+
+  return (
+    <>
+      <DashNav />
+      <SideBar />
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Dashboard</h1>
+      </div>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>EMAIL</th>
+                <th>ADMIN</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user._id}>
+                  <td>{user._id}</td>
+                  <td>{user.name}</td>
+                  <td>
+                    <a href={`mailto: ${user.email}`}> {user.email}</a>
+                  </td>
+                  <td>
+                    {user.isAdmin ? (
+                      <i
+                        className="fas fa-check"
+                        style={{ color: "green" }}
+                      ></i>
+                    ) : (
+                      <i className="fas fa-times" style={{ color: "red" }}></i>
+                    )}
+                  </td>
+                  <td>
+                    <Link to={`/dashboard/user/${user._id}/edit`}>
+                      <Button className="btn-sm" variant="light">
+                        <i className="fas fa-edit"></i>
+                      </Button>
+                    </Link>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => deleteUserHandler(user._id)}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </main>
+    </>
+  );
+};
+
+export default UserList;
