7552a476e2b77dbfd5aaf870e557e4091a9df81a
natnael meresa <natnaelmeresa6@gmail.com>
2021-12-01 12:15:07 +0300

    changes in the backend

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,22 @@ import express from 'express'
 import dotenv from 'dotenv';
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 import {notFound, errorHandler} from './middleware/error.js'
 
 dotenv.config()
 connectDB()
 
 const app =  express();
+app.use(express.json())
 
 
 
 app.use('/api/products', productRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
 app.use(notFound)
 
