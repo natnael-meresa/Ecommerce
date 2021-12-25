e68b7045917f54597efeef2a00c8b83cc49a58de
natnael meresa <natnaelmeresa6@gmail.com>
2021-12-25 23:30:25 +0300

    changes carousel to homepage

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -10,23 +10,37 @@ const ProductCarousel = () => {
 
     const productTopRated = useSelector(state => state.productTopRated)
     const { loading, error, products } = productTopRated
-
+    let product = {_id: '1', name:'name'}
     useEffect(() => {
         dispatch(listTopProducts())
     }, [dispatch])
     
     return loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
-        <Carousel pause='hover' className='bg-dark'>
-            {products.map(product => (
+        <Carousel pause='true' className='bg-dark carsoual carousel-content'>
                 <Carousel.Item key={product._id}>
-                    <Link to={`/product/${product._id}`}>
-                        <Image src={product.image} alt={product.name} />
+                    <Link to={`/product/${product._id}`} >
+                        <Image src="./images/slid3.jpg" alt={product.name} />
                         <Carousel.Caption className='carousel-caption'>
-                            <h2>{product.name} {product.price}</h2>
+                            <h2>we provide the best products with cheepest prices</h2> 
                         </Carousel.Caption>
                     </Link>
                 </Carousel.Item>
-            ))}
+                <Carousel.Item key={product._id}>
+                    <Link to={`/product/${product._id}`} >
+                        <Image src="./images/slid4.jpg" alt={product.name} />
+                        <Carousel.Caption className='carousel-caption'>
+                        <h2>we provide the best products with cheepest prices</h2> 
+                        </Carousel.Caption>
+                    </Link>
+                </Carousel.Item>
+                <Carousel.Item key={product._id}>
+                    <Link to={`/product/${product._id}`} >
+                        <Image src="./images/slid5.jpg" alt={product.name} />
+                        <Carousel.Caption className='carousel-caption'>
+                        <h2>we provide the best products with fastest delivery</h2>  
+                        </Carousel.Caption>
+                    </Link>
+                </Carousel.Item>
         </Carousel>
     )
 }
